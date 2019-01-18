6cb0c1fb942afc7eafec33941ce85c5996d99573
rajesh <rajesh.kodati@gmail.com>
2019-01-18 09:26:13 +0530

    create dynamic p

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("hello")
+function myFunction() {
+    setInterval(function(){
+        var i=0;
+        var x = document.createElement("P");
+        
+    var t = document.createTextNode(i);
+    x.appendChild(t);
+    document.body.appendChild(x);
+    
+    },1000)
+    
+  }
+  myFunction();
